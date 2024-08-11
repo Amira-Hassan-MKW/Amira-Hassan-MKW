81e5684df1a3dab9413da6fa38e30a6a61046bf4
Amira-Hassan-MKW <hsmlahor@gmail.com>
2024-08-11 05:47:23 +0500

    Create Green.m

---
+++ Green.m
@@ -0,0 +1,13 @@
+- 👋 Hi, I’m @Amira-Hassan-MKW
+- 👀 I’m interested in 
+- 🌱 I’m currently learning 
+- 💞️ I’m looking to collaborate on
+- 
+- 📫 How to reach m💕💕💕♻️(*￣∇￣*)e ...
+- 😄 Pronouns:  the arrows to rotate the object to face in the direction of the hand. (2 of 3)
+- ⚡ Fun fact: ...
+
+<!---
+Amira-Hassan-MKW/Amira-Hassan-MKW is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
